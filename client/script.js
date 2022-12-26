0729840417d5329912d59672673bf3a2c7bf7669
Shrihari Maheshwari <shrihari.maheshwari7@gmail.com>
2022-12-26 19:24:42 +0530

    deployed in render.com

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async(e) => {
   loader(messageDiv)
 
   // fetch data from the server -> bot's response
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://maverick.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
